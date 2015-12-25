e40881e950ba6d4afe70461c7d23926bb15b2726
Valery Girkin <vary10@gmail.com>
2015-12-25 23:58:06 +0300

    Update server.cpp

--- server/server.cpp
+++ server/server.cpp
@@ -10,13 +10,13 @@ void Server::my_serve() {
 
     SocketReader from_client(client->get_sd());
     SocketWriter to_client(client->get_sd());
-//    RedisValue err = RedisError("I'm not a real redis");
+    RedisValue err = RedisError("I'm not a real redis");
     while (true) {
         RedisValue val;
         ReadRedisValue(&from_client, &val);
         std::cout << "read " << std::endl;
-//        WriteRedisValue(&to_client, err);
-        WriteRedisValue(&to_client, val);
+        WriteRedisValue(&to_client, err);
+        // WriteRedisValue(&to_client, val);
         to_client.flush();
         std::cout << "wrote " << std::endl;
         from_client.read_more();
