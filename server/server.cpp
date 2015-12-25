0fad998aa1882a40d2824e9792b1c4d2fa5fd076
Valery Girkin <vary10@gmail.com>
2015-12-25 23:05:25 +0300

    fin

--- server/server.cpp
+++ server/server.cpp
@@ -5,16 +5,17 @@ Server::Server(int port, unsigned int clients) : my_listener(port, clients) {}
 void Server::my_serve() {
 
     std::cout << "start" << std::endl;
-    // std::vector<std::unique_ptr<Socket>> clients;
     std::unique_ptr<Socket> client = my_listener.accept_client();
     std::cout << "got client" << std::endl;
 
     SocketReader from_client(client->get_sd());
     SocketWriter to_client(client->get_sd());
+//    RedisValue err = RedisError("I'm not a real redis");
     while (true) {
         RedisValue val;
         ReadRedisValue(&from_client, &val);
         std::cout << "read " << std::endl;
+//        WriteRedisValue(&to_client, err);
         WriteRedisValue(&to_client, val);
         to_client.flush();
         std::cout << "wrote " << std::endl;
